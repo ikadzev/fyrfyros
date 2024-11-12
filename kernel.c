fca889e6f8e5d621be4abe94d5fdba09e00a1e7c
ikadzev <ikadzev@mail.ru>
2024-11-12 15:53:35 +0700

    fix header

--- kernel.c
+++ kernel.c
@@ -1,32 +1,33 @@
 #include "source/headers/vga_driver.h"
 #include "source/headers/printf.h"
 #include "source/headers/kernel_allocator.h"
+#include "source/headers/tramplins.h"
 
 void print_logo();
 
-static void create_lidt();
+// static void create_lidt();
 
 
-void interrupt_call(int interrupt_number) {
-    __asm__ __volatile__ (
-            "int %0\n\t"  // Вставка прерывания
-            :
-            : "i"(interrupt_number)  // Прерывание передается как немедленное значение
-            );
-}
+// void interrupt_call(int interrupt_number) {
+//     __asm__ __volatile__ (
+//             "int %0\n\t"  // Вставка прерывания
+//             :
+//             : "i"(interrupt_number)  // Прерывание передается как немедленное значение
+//             );
+// }
 
 void kernel_entry() {
     vga_clear_screen();
     kernel_start_allocator();
     create_lidt();
-    interrupt_call(12);
-    //int a = 4/0;
+    // interrupt_call(12);
+    int a = 4/0;
     //macro(12)
     for (;;);
 }
 
 
-#include "source/headers/tramplins.h"
+
 
 void print_logo() {
     vga_clear_screen();
