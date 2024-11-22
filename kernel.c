a37c401186bf7d67670c28c098c7816417f89d70
ikadzev <ikadzev@mail.ru>
2024-11-22 10:34:57 +0700

    .asm in makefile

--- kernel.c
+++ kernel.c
@@ -5,18 +5,20 @@
 #include "source/headers/intel8259A.h"
 
 void print_logo();
+void asm_test();
 
 void kernel_entry() {
     vga_clear_screen();
     kernel_start_allocator();
     create_lidt();
+    // asm_test();
     configurate_intel8258A_all();
     __asm__ __volatile__ (
             ".intel_syntax noprefix\n\t"
             "sti\n\t"
             ".att_syntax prefix\n\t"
             );
-    //print_logo();
+    // print_logo();
     for (;;);
 }
 void print_logo() {
---
+++ source/test.asm
@@ -0,0 +1,6 @@
+[BITS 32]
+
+[GLOBAL _asm_test]
+
+_asm_test:
+    int 0x42
